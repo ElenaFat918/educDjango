a9977bc0a8cb837149eea31032733f9333e050b4
Elena <keaya@rambler.ru>
2023-08-19 19:53:36 +0300

    лекция и семинар 1

---
+++ seminar_1_project/firstproject/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('coin/', views.coin, name='coin'),
+    path('cube/', views.cube, name='cube'),
+    path('rand100/', views.rand100, name='rand100'),
+]
