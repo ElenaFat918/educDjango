6db0bdf921ee1ec3808ccb712f3a63980421d338
ElenaFat918 <109763007+ElenaFat918@users.noreply.github.com>
2023-08-19 19:28:07 +0300

    Delete lessonproject directory

--- lessonproject/lessonapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-"""cоединяем маршруты и представления"""
-urlpatterns = [
-    path('', views.index, name='index'),   # по пустому пути импортирую функцию index из модуля index и называю её index
-    path('about/', views.about, name='about'),  # по пути about вызываю функцию about из модуля about и называю её about
-]
