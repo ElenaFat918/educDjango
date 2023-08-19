195f8ac331b77d7807c2d7983e0189fca8745270
ElenaFat918 <109763007+ElenaFat918@users.noreply.github.com>
2023-08-19 19:28:21 +0300

    Delete seminar_1_project directory

--- seminar_1_project/firstproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('coin/', views.coin, name='coin'),
-    path('cube/', views.cube, name='cube'),
-    path('rand100/', views.rand100, name='rand100'),
-]
